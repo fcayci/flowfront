ce93caf4722b77f4e4b7f4f09595fa9a96bda51c
Furkan Cayci <furkanca02@gmail.com>
2018-12-13 22:53:12 +0300

    perturb1d added multi runs

--- perturb1d.py
+++ perturb1d.py
@@ -34,78 +34,88 @@ np.set_printoptions(suppress=True)
 DEBUG = False
 
 x_length = 0.7
-num_of_nodes_x = 36
-num_of_nodes_y = 10
+num_of_nodes_x = 2600
+num_of_nodes_y = 100
 A = 1
 B = 1
-kxx = 124.319
 
-target = flowgen1d(x_length, num_of_nodes_x, num_of_nodes_y, kxx, A)
+n_of_runs = 100
 
-if DEBUG:
-   print('target flowfront:', target[0,:])
-   #show_img(target)
+for t in range(n_of_runs):
 
-# solver
-########
+    kxx = (np.random.random() + 1) * 100
+    if DEBUG:
+        print('choose kxx:', kxx)
 
-# gradient descent
-# 1. perturb for direction
-# 1. jump for faster converge
-# kxx(t+1) = kxx(t) - gain * F(J)
+    target = flowgen1d(x_length, num_of_nodes_x, num_of_nodes_y, kxx, A)
 
-# minimizing J (norm difference between vectors)
-score = 0
-prev_score = 0
+    if DEBUG:
+        print('target flowfront:', target[0,:])
+    #show_img(target)
 
-threshold = 1
-gamma_l = 10
-gamma_s = 2.4
-kxx = 1 # starting point
-prev_kxx = 0
-perturb = 0
-gain = 0
-diff_score = 0
-maxscore = 0
-A = 1
+    # solver
+    ########
 
-nodes = np.linspace(0, x_length, num_of_nodes_x)
-s = np.zeros((num_of_nodes_y, num_of_nodes_x), dtype=float)
+    # gradient descent
+    # 1. perturb for direction
+    # 1. jump for faster converge
+    # kxx(t+1) = kxx(t) - gain * F(J)
 
-n_of_steps = 10000
+    # minimizing J (norm difference between vectors)
+    score = 0
+    prev_score = 0
 
-for step in range(n_of_steps):
-    # calculate the new s vectors
-    for i, node in enumerate(nodes):
-        s[:,i] = darcy1d(node, kxx, A)
-    # save previous score
-    prev_score = score
-    # calculate new score
-    score = l2norm(s, target)
-    maxscore = max(score, maxscore)
-    p_score = np.sign(score - prev_score)  * (score / (0.1 + maxscore))
-    print("{:2}, ps: {:8.2f}, cs: {:8.2f}, kxx: {:14.6f}, p_score: {:14.6f}".format(step, prev_score, score, kxx, p_score))
+    threshold = 0.001
+    gamma_l = 0.7
+    gamma_s = 0.2
+    kxx = 1 # starting point
+    prev_kxx = 0
+    perturb = 0
+    gain = 0
+    diff_score = 0
+    maxscore = 0
+    A = 1
 
-    if score < threshold:
-        print("SUCCESS: achieved threshold < {}".format(threshold))
-        break
+    nodes = np.linspace(0, x_length, num_of_nodes_x)
+    s = np.zeros((num_of_nodes_y, num_of_nodes_x), dtype=float)
 
-    if step % 2 == 0:
-        # small perturb to figure out if going the correct direction
-        perturb = gamma_s * kxx * (np.random.random() - 0.5) * p_score
-        kxx = kxx - perturb
-        if DEBUG:
-            print("pert: {}, kxx: {}".format(perturb, kxx))
+    n_of_steps = 200
 
+    for step in range(n_of_steps):
+        # calculate the new s vectors
+        for i, node in enumerate(nodes):
+            s[:,i] = darcy1d(node, kxx, A)
+        # save previous score
+        prev_score = score
+        # calculate new score
+        score = l2norm(s, target)
+        maxscore = max(score, maxscore)
+        p_score = np.sign(prev_score - score)  * (score / (0.1 + maxscore))
+        #print("{:2}, ps: {:8.2f}, cs: {:8.2f}, kxx: {:14.6f}, p_score: {:14.6f}".format(step, prev_score, score, kxx, p_score))
+
+        if score < threshold:
+            #print("SUCCESS: achieved threshold < {}".format(threshold))
+            break
+
+        if step % 2 == 0:
+            # small perturb to figure out if going the correct direction
+            perturb = gamma_s * kxx * (np.random.random() - 0.5) * p_score
+            kxx = kxx - perturb
+            if DEBUG:
+                print("pert: {}, kxx: {}".format(perturb, kxx))
+
+        else:
+            # jump the direction
+            #diff_score = (score - prev_score) / max(score, prev_score)
+            gain = np.sign(perturb) * gamma_l * kxx * p_score #diff_score
+            kxx = kxx - gain
+            if DEBUG:
+                print("leap: {}, diff score: {}, kxx: {}".format(gain, diff_score, kxx))
     else:
-        # jump the direction
-        #diff_score = (score - prev_score) / max(score, prev_score)
-        gain = gamma_l * perturb * p_score #diff_score
-        kxx = kxx + gain
-        if DEBUG:
-            print("leap: {}, diff score: {}, kxx: {}".format(gain, diff_score, kxx))
+        print("FAIL: could not find...")
+        break
 else:
-    print("FAIL: could not find...")
+    print("SUCCESS: solved {} runs with {} threshold achievement".format(n_of_runs, threshold))
 
-print("Target Combo:", target[0,:])
-print("Winning Combo:", s[0,:])
+    #print("Target Combo:", target[0,:])
+    #print("Winning Combo:", s[0,:])
