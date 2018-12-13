94d9017f8615a5240f0ea6221922f144c9eef095
Furkan Cayci <furkanca02@gmail.com>
2018-12-13 22:52:55 +0300

    perturb2d is working around 1.7k steps

--- perturb2d.py
+++ perturb2d.py
@@ -64,10 +64,10 @@ if DEBUG:
 score = 0
 prev_score = 0
 
-threshold = 1
-gamma_l = 20
-gamma_s = 1.4
-kxx = 1 # starting point
+threshold = 0.01
+gamma_l = 0.7
+gamma_s = 0.2
+kxx = 312.421 # starting point
 kyy = 1 # starting point
 prev_kxx = 0
 prev_kyy = 0
@@ -84,9 +84,17 @@ nodes_x = np.linspace(0, x_length, num_of_nodes_x)
 nodes_y = np.linspace(0, y_length, num_of_nodes_y)
 s = np.zeros((num_of_nodes_y, num_of_nodes_x), dtype=float)
 
-n_of_steps = 10
+n_of_steps = 10000
+
+# keep optimization on single paramter
+# and swap parameters every so often (swaptime steps)
+swaptime = 100
+xflag = False
 
 for step in range(n_of_steps):
+
+    if step % swaptime == 0:
+        xflag = not xflag
     # This part will be handed off to LIMS
     #   We will get the new s matrix in return
     # calculate the new s vectors
@@ -98,13 +106,12 @@ for step in range(n_of_steps):
     prev_score = score
     # calculate new score
     score = l2norm(s, target)
-    if score < 0.01:
-        maxscore = 1
-    elif score < 1:
-        maxscore = 10
+    # if score < 0.01:
+    #     maxscore = 1
+    # elif score < 1:
+    #     maxscore = 10
     maxscore = max(score, maxscore)
-    p_score = np.sign(score - prev_score) * (score / (0.1 + maxscore))
-    print("{:6}, ps: {:8.2f}, cs: {:8.2f}, kxx: {:14.6f}, kyy: {:14.6f}, p_score: {:14.6f}".format(step, prev_score, score, kxx, kyy, p_score))
+    p_score = np.sign(prev_score - score)  * (score / (0.1 + maxscore))
 
     if score < threshold:
         print("SUCCESS: achieved threshold < {}".format(threshold))
@@ -112,25 +119,30 @@ for step in range(n_of_steps):
 
     if step % 2 == 0:
         # small perturb to figure out if going the correct direction
-        perturbx = gamma_s * kxx * (np.random.random() - 0.5) * p_score
-        kxx = kxx - perturbx
-        perturby = gamma_s * kyy * (np.random.random() - 0.5) * p_score
-        kyy = kyy - perturby
+        if xflag:
+            perturbx = gamma_s * kxx * (np.random.random() - 0.5) * p_score
+            kxx = kxx - perturbx
+        else:
+            perturby = gamma_s * kyy * (np.random.random() - 0.5) * p_score
+            kyy = kyy - perturby
         if DEBUG:
             print("pertx: {}, perty: {}, kxx: {}, kyy: {}".format(perturbx, perturby, kxx, kyy))
 
     else:
         # jump the direction
-        #diff_score = (score - prev_score) / max(score, prev_score)
-        gainx = gamma_l * perturbx * p_score #diff_score
-        kxx = kxx + gainx
-        gainy = gamma_l * perturby * p_score #diff_score
-        kyy = kyy + gainy
+        if xflag:
+            gainx = np.sign(perturbx) * gamma_l * kxx * p_score
+            kxx = kxx - gainx
+        else:
+            gainy = np.sign(perturby) * gamma_l * kyy * p_score
+            kyy = kyy - gainy
         if DEBUG:
             print("leapx: {}, leapy: {}, kxx: {}, kyy: {}".format(gainx, gainy, kxx, kyy))
+
+    print("{:6}, ps: {:8.2f}, cs: {:8.2f}, kxx: {:14.6f}, kyy: {:14.6f}, pertx: {:10.4f}, perty: {:10.4f}, gainx: {:10.4f}, gainy: {:10.4f}, p_score: {:10.4f}".format(step, prev_score, score, kxx, kyy, perturbx, perturby, gainx, gainy, p_score))
 
 else:
     print("FAIL: could not find...")
 
-print("Target Combo:", target[0,:])
-print("Winning Combo:", s[0,:])
+#print("Target Combo:", target[0,:])
+#print("Winning Combo:", s[0,:])
