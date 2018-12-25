231b0abb0a6e64b0f889997612925ee74695a1a9
Furkan Cayci <furkanca02@gmail.com>
2018-12-25 22:39:52 +0300

    cleaned up show flow front code

--- showflow.py
+++ showflow.py
@@ -6,7 +6,7 @@ from common import *
 from lims_common import *
 
 BOARDSIZE = (0.2, 0.7) # board size in meters (y, x)
-NODESIZE = (11, 36) # number of nodes in each direction (y, x)
+NODESIZE  = (11, 36)   # number of nodes in each direction (y, x)
 
 #### Create target flowfront
 
@@ -15,17 +15,18 @@ c = Coeffs(mu=0.1, fi=0.5, deltaP=1e5)
 
 # create permeability map instance
 # elements can be accessed by p.kxx
-p_t = PMap(kxx=1e-10, kyy=1e-10)
+#p = PMap(kxx=1e-10, kyy=1e-10, kxy=1e-12, krt=1e-9)
+p = PMap(kxx=1e-10,  krt=1e-9)
 # randomize if needed
-#p_t.randomize()
+#p.randomize(lower=1e-14, upper=1e-8)
 
-# create gate locations
-gatelocs = set_gatenodes(NODESIZE, 'sw')
+# set up the gates
+# w  : west
+# nw : north west
+# sw : south west
+gatenodes = set_gatenodes(NODESIZE, 'w')
+
 # calculate target flow time
-ft_l = lims_flowtime(BOARDSIZE, NODESIZE, p_t, c, 'run1', gatelocs)
-#ft_p = calculate_flowtime(BOARDSIZE, NODESIZE, p_t, c, 'run1', gatelocs)
-print('lims', ft_l)
-#print('pith', ft_p)
-show_img(ft_l)
-#show_img(ft_p)
-#show_imgs(ft_l, ft_p)
+ft = lims_flowtime(BOARDSIZE, NODESIZE, p, c, 'target', gatenodes)
+print('lims', ft)
+show_img(ft)
